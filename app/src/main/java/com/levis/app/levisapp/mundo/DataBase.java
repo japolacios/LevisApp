9562238a8ac95c9c11c8a8b629a698c9228b3cdb
juancamilovilladagamboa <jncvg17@hotmail.com>
2017-09-17 15:19:03 -0500

    Se agregaron las clases DataBase y LogicDataBase aun sin modificar

---
+++ app/src/main/java/com/levis/app/levisapp/mundo/DataBase.java
@@ -0,0 +1,50 @@
+package com.levis.app.levisapp.mundo;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Laboratorioi2t on 15/08/2017.
+ */
+
+public class DataBase {
+    /**
+     * Contains the name of the table to create that contains the row counters.
+     */
+    public static final String TABLA_JUGADORES = "jugadores";
+
+    /**
+     * Contains the SQL query to use to create the table containing the row counters.
+     */
+    public static final String SQL_CREATE_TABLE = "CREATE TABLE "
+            + DataBase.TABLA_JUGADORES + " ("
+            + DatosColumnas.JUGADOR_NOMBRE + " TEXT,"
+            + DatosColumnas.JUGADOR_APELLIDO + " TEXT,"
+            + DatosColumnas.JUGADOR_IDENTIFICACION + " TEXT PRIMARY KEY,"
+            + DatosColumnas.JUGADOR_TIPO_IDENTIFICACION + " TEXT,"
+            + DatosColumnas.JUGADOR_EMAIL + " TEXT,"
+            + DatosColumnas.JUGADOR_CONTRASENA + " TEXT,"
+            + DatosColumnas.JUGADOR_PARTIDAS_JUGADAS + " INT,"
+            + DatosColumnas.JUGADOR_PARTIDAS_GANADAS + " INT,"
+            + DatosColumnas.JUGADOR_RESPUESTAS_CORRECTAS + " INT,"
+            + DatosColumnas.JUGADOR_RESPUESTAS_INCORRECTAS + " INT)";
+
+
+    /**
+     * This class represents the rows for an entry in the row_counter table. The
+     * primary key is the _id column from the BaseColumn class.
+     */
+    public static abstract class DatosColumnas implements BaseColumns {
+
+        public static final String JUGADOR_NOMBRE = "nombre";
+        public static final String JUGADOR_APELLIDO = "apellido";
+        public static final String JUGADOR_IDENTIFICACION = "identificacion";
+        public static final String JUGADOR_TIPO_IDENTIFICACION = "tipo_identificacion";
+        public static final String JUGADOR_EMAIL = "email";
+        public static final String JUGADOR_CONTRASENA = "contrasena";
+        public static final String JUGADOR_PARTIDAS_JUGADAS = "partidas_jugadas";
+        public static final String JUGADOR_PARTIDAS_GANADAS = "partidas_ganadas";
+        public static final String JUGADOR_RESPUESTAS_CORRECTAS = "correctas";
+        public static final String JUGADOR_RESPUESTAS_INCORRECTAS = "incorrectas";
+
+    }
+}
