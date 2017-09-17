79e0b695b19d7efa0e94f0909191b8b35f758db6
Japo <japolacios@gmail.com>
2017-09-17 00:34:17 -0500

    FistApproach to seach view - missing list layout

--- app/src/main/java/com/levis/app/levisapp/interfaz/Search.java
+++ app/src/main/java/com/levis/app/levisapp/interfaz/Search.java
@@ -2,14 +2,69 @@ package com.levis.app.levisapp.interfaz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.levis.app.levisapp.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Search extends AppCompatActivity {
+
+
+    //Set ListView Components
+    ListView listView;
+    List list = new ArrayList();
+    ArrayAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        //Get listView By Id
+        listView = (ListView) findViewById(R.id.main_list);
+
+
+        //Add items in the meantime
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+        list.add("HolaQueHace");
+
+
+        //Load layout and data to listView
+        adapter = new ArrayAdapter(Search.this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
     }
+
 }
