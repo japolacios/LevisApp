2110dc776cb94b40e910ef701a6bc949d6383dcc
juancamilovilladagamboa <jncvg17@hotmail.com>
2017-09-18 08:38:26 -0500

    Se añadio una libreria para la captura de Imagenes

--- app/src/main/java/com/levis/app/levisapp/interfaz/Register.java
+++ app/src/main/java/com/levis/app/levisapp/interfaz/Register.java
@@ -2,10 +2,6 @@ package com.levis.app.levisapp.interfaz;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.provider.*;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,9 +9,19 @@ import android.widget.EditText;
 
 import com.levis.app.levisapp.R;
 import com.levis.app.levisapp.mundo.HWDPrincipal;
+import com.levis.app.levisapp.mundo.Imagen;
 import com.levis.app.levisapp.mundo.LogicDataBase;
 import com.levis.app.levisapp.mundo.SessionManagement;
 import com.levis.app.levisapp.mundo.Usuario;
+import com.nguyenhoanglam.imagepicker.model.Config;
+import com.nguyenhoanglam.imagepicker.model.Image;
+import com.nguyenhoanglam.imagepicker.ui.imagepicker.ImagePicker;
+
+import java.nio.ByteBuffer;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Register extends Activity {
 
@@ -25,6 +31,7 @@ public class Register extends Activity {
     // Session Manager Class
     SessionManagement session;
     private LogicDataBase db;
+    private ArrayList<Image> images = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +47,7 @@ public class Register extends Activity {
         nombre=(EditText)findViewById(R.id.name);
         correo=(EditText)findViewById(R.id.mail);
         contraseña=(EditText)findViewById(R.id.password);
-        imagenBoton=(Button)findViewById(R.id.botonimagen) ;
+        imagenBoton=(Button)findViewById(R.id.botonimagen);
 
         registrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +60,20 @@ public class Register extends Activity {
                 user.setNombreUsuario(nombree);
                 user.setCorreoElectronico(email);
                 user.setUsuPassword(pass);
+
+                if(images.size()>0){
+                    Imagen img = new Imagen();
+                    img.setNombreUsuario(nombree);
+                    DateFormat df = new SimpleDateFormat("EEE, MMM d, yyyy");
+                    String date = df.format(Calendar.getInstance().getTime());
+                    img.setFechaCarga(date);
+                    img.setTitulo("imagenPerfil");
+                    img.setCorreoUsuario(email);
+                    Image img1 = images.get(0);
+                    ByteBuffer buffer = img1.getPlanes()[0].getBuffer();
+                    byte[] bytes = new byte[buffer.capacity()];
+                    img.setImagenTabla();
+                }
 
                 db.insertarUsuario(user);
 
@@ -77,43 +98,31 @@ public class Register extends Activity {
         imagenBoton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(i , 0);
+                start();
             }
         });
 
 
 
     }
-    //Start the camera to take a picture
-    public void openCamera(View view) {
 
+    private void start() {
+        ImagePicker.with(this)
+                .setFolderMode(true)
+                .setCameraOnly(false)
+                .setFolderTitle("Album")
+                .setMultipleMode(false)
+                .setSelectedImages(images)
+                .setMaxSize(10)
+                .start();
     }
-    //What to do after the picture is taken
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    Intent resultData) {
-        super.onActivityResult(requestCode, resultCode, resultData);
-
-        if (resultData != null) {
-
-            String[] projection = {MediaStore.Images.Media.DATA};
-            Cursor cursor = managedQuery(
-                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                    projection, null, null, null);
-            int column_index_data = cursor
-                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToLast();
-
-            String imagePath = cursor.getString(column_index_data);
-            Bitmap bitmapImage = BitmapFactory.decodeFile(imagePath);
-            //Log.e("DEV", "Se supone que el path es este: " + imagePath);
-            //imageView.setImageBitmap(bitmapImage );
-
-            System.out.println(bitmapImage);
-
-
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Config.RC_PICK_IMAGES && resultCode == RESULT_OK && data != null) {
+            images = data.getParcelableArrayListExtra(Config.EXTRA_IMAGES);
         }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
 
